e215b1de7e986ba243fc48f8246adb967a91467f
Akshata <akshata.meti@terralogic.com>
2021-06-08 09:14:51 +0530

    initial update

---
+++ models/Transfer.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema
+let Transfer = new Schema({
+   
+   bank: {
+      type: String
+   },
+   AccountNo: {
+      type: String
+   },
+   ifsccode: {
+    type: String
+   },
+   name: {
+    type: String
+   },
+}, {
+   collection: 'transfer'
+})
+
+module.exports = mongoose.model('Transfer', Transfer)
